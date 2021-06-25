214986aa62ddc06798ab09d73d19eb1dfa8c3719
Julian Kaffke <julian.kaffke@gmail.com>
2021-06-25 16:26:52 +0200

    Improved and working display and DHT22

--- boot.py
+++ boot.py
@@ -2,6 +2,8 @@
 import senko
 import machine
 import network
+import gc
+import micropython
 
 def do_connect():
     sta_if = network.WLAN(network.STA_IF)
@@ -20,15 +22,20 @@ def do_connect():
     ntptime.settime() # set the rtc datetime from the remote server
     print(rtc.datetime())
 
+# print('free:', str(gc.mem_free()))
+# # print('info:', str(machine.info()))
+# print('info:', str(gc.mem_alloc()))
+# print('info:', str(micropython.mem_info()))
+# gc.collect()
 
 
-OTA = senko.Senko(
-  user="jaykay", repo="esp32", files = ["boot.py", "main.py"]
-)
+# OTA = senko.Senko(
+#   user="jaykay", repo="esp32", files = ["boot.py", "main.py"]
+# )
 
 # Connect to Wi-Fi network.
-do_connect()
+# do_connect()
 
-if OTA.update():
-    print("Updated to the latest version! Rebooting...")
-    machine.reset()
+# if OTA.update():
+#     print("Updated to the latest version! Rebooting...")
+#     machine.reset()
--- main.py
+++ main.py
@@ -1,19 +1,54 @@
-from machine import Pin
+
+from machine import Pin, SPI
 from time import sleep
+import ssd1306
+import dht
 
 led = Pin(2, Pin.OUT)
+dht22 = Pin(33)
+hspi = SPI(1)  # sck=14 (scl), mosi=13 (sda), miso=12 (unused)
+dc = Pin(4)    # data/command
+rst = Pin(5)   # reset
+cs = Pin(15)   # chip select, some modules do not have a pin for this
+d = dht.DHT22(dht22)
+
+display = ssd1306.SSD1306_SPI(128, 64, hspi, dc, rst, cs)
+
+MIN_TEMP = 26.0
+MAX_TEMP = 30.0
+
+
+def status(temp):
+  if MIN_TEMP <= temp <= MAX_TEMP:
+    return "OK"
+  return "NOK"
+
+
+
+# $$$$$$$$$$$$$$$$
+# ----------------
+# FFFFFFFFFFFFFFFF
+# FFFFFFFFFFFFFFFF
+# FFFFFFFFFFFFFFFF
+# FFFFFFFFFFFFFFFF
+# FFFFFFFFFFFFFFFF
 
 while True:
-  led.value(not led.value())
-  sleep(3.0)
+  d.measure()
+  temp = d.temperature()
+  hum = d.humidity()
 
-#   import esp32
-#   print("Hall:")
-#   print(esp32.hall_sensor())    # read the internal hall sensor
-#   print("Temperature")
-#   celsius = (esp32.raw_temperature() - 32) * 5/9
-#   print(celsius)
+  tempStr = "Temp.: {}C".format(temp)
+  humStr = "Humi.: {}%".format(hum)
 
-import upip
-upip.help()
-upip.install(package_or_package_list, [path])
+  display.fill(0)
+  display.contrast(10)
+  display.invert(1)
+  display.text("Status:      {}".format(status(temp)), 2, 4, 1)
+  display.text(tempStr, 2, 18, 1)
+  display.text(tempStr, 2, 26, 1)
+  display.text(tempStr, 2, 34, 1)
+  display.text(humStr, 2, 42, 1)
+  display.text(humStr, 2, 50, 1)
+  display.show()
+  sleep(5.0)
