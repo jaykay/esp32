c0c547f0aee25fa9efe5634ac83628cdafac6e60
Julian Kaffke <julian@42nerds.com>
2021-06-25 12:02:23 +0200

    Blubb

--- boot.py
+++ boot.py
@@ -1,5 +1,9 @@
+# boot.py
+import senko
+import machine
+import network
+
 def do_connect():
-    import network
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
@@ -9,12 +13,22 @@ def do_connect():
             pass
     print('network config:', sta_if.ifconfig())
     print('Setting clock to NTP')
-    from machine import RTC
 
-    rtc = RTC()
+    rtc = machine.RTC()
 
     import ntptime
     ntptime.settime() # set the rtc datetime from the remote server
     print(rtc.datetime())
 
+
+
+OTA = senko.Senko(
+  user="jaykay", repo="esp32", files = ["boot.py", "main.py"]
+)
+
+# Connect to Wi-Fi network.
 do_connect()
+
+if OTA.update():
+    print("Updated to the latest version! Rebooting...")
+    machine.reset()
